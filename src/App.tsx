b6e5513e9f51dcf37e17bd16a21cf13d39b0fa6b
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-11-08 23:50:22 -0800

    all done tweaking for the day

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
 import * as React from "react";
-import {
-  makeStyles,
-  Card,
-  CardPreview,
-} from "@fluentui/react-components";
-
-
+import { makeStyles, Card, CardHeader } from "@fluentui/react-components";
+import { Database20Filled } from "@fluentui/react-icons";
 
 const useStyles = makeStyles({
   card: {
     margin: "auto",
     width: "250px",
     height: "350px",
- 
+    color: "pink",
+    padding: 0,
+    borderRadius: "20px",
   },
   cardContainer: {
     display: "flex",
     backgroundColor: "white",
     marginLeft: "2rem",
-    padding: "30px", 
+    padding: "30px",
   },
- 
+  headerRoot: {
+    display: "flex",
+    backgroundColor: "yellowgreen",
+    height: "50%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  iconWrapper: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  iconStyle: {
+    width: 48,
+    height: 48,
+    fill: "white", // Use fill for SVG icon color to show on dark background
+  },
 });
 
 const App = (): JSX.Element => {
@@ -29,10 +42,11 @@ const App = (): JSX.Element => {
   return (
     <div className={styles.cardContainer}>
       <Card className={styles.card}>
- 
-        <CardPreview></CardPreview>
-        <CardHeader></CardHeader>
-  
+        <CardHeader as="div" className={styles.headerRoot}>
+         <div>Hi</div>
+        </CardHeader> <div className={styles.iconWrapper}>
+            <Database20Filled className={styles.iconStyle} />
+          </div>
       </Card>
     </div>
   );
